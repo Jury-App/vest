4bc68d8b80896f541b434ac7b9560e647168a1ea
nicoleym <153690412+nicoleym@users.noreply.github.com>
2026-04-03 15:45:49 -0700

    Copy

--- app/components/ObjectsHero.tsx
+++ app/components/ObjectsHero.tsx
@@ -931,89 +931,84 @@ export default function ObjectsHero({ onInvest }: { onInvest: () => void }) {
             }}
           >
             <div ref={firstBodyCopyRef}>
+            <p style={{ marginTop: 48 }}>Hi friend,</p>
+              <br />
               <p>
-                Maybe you know from a past life, through a mutual, maybe we&apos;re new friends. I&apos;m a South
-                Korean country girl at heart with the privilege of growing up in
-                cities around the world because my Swiss dad was a chef at fancy
-                hotels. When I moved to the US for college, I became the first in
-                my line to graduate high school, get a bachelor&apos;s degree - and
-                through many mistakes slash happy accidents, work my way into
-                Silicon Valley. Long story short, I make apps and websites. Previously at Code &
-                Theory, Mayvenn Hair, Credit Karma, and Genies. Recently, by the
-                grace of God, luck, and capitalism - for myself.
+                Maybe you know from a past life, through a mutual, maybe we&apos;re newly acquainted. I&apos;m a South Korean country girl at heart with the privilege of growing up in cities around the world because my Swiss dad was a chef at fancy hotels. When I moved to the US for college, I became the first in my line to graduate high school, get a bachelor&apos;s degree - and through many mistakes/happy accidents, worked my way into Silicon Valley. Long story short, I make apps and websites. Previously at Code & Theory, Mayvenn Hair, Credit Karma, and Genies. Recently, by the grace of God, luck, and capitalism - for myself.
               </p>
-              <p style={{ marginTop: 24 }}>
-                Along the way I noticed some paradoxical truths;
+              <br />
+              <p style={{ marginTop: 0 }}>
+             Along the way I noticed some paradoxical truths;
               </p>
-
-              <ol className="list-decimal space-y-4 pl-5" style={{ marginTop: 24 }}>
+              <ol
+                className="list-decimal space-y-4 pl-6"
+                style={{ marginTop: 24, marginLeft: isMobile ? 12 : 24 }}
+              >
                 <li>
                   <p>
-                    Good technology is magic. It is literal witchcraft but bad
-                    technology drains the soul out of you and wastes your fucking
-                    time. See; filing unemployment (why do I have to input the same
-                    job I&apos;m emailing every week?), insurance claims, or any
-                    other adult activity that&apos;s not sex-related.
+                    <strong>
+                      Good technology is magic but bad technology drains your
+                      soul and wastes your fucking time.
+                    </strong>{" "}
+                    See; filing unemployment <em>(why do I have to input the same job I&apos;m emailing every week?)</em>, insurance claims, taxes, etc. <br /><br />
                   </p>
                 </li>
                 <li>
                   <p>
-                    A lot of technology businesses make a fuck tonne of money
+                   <strong>For some reason, the unbridled scaling power of software comes at the cost of a good user experience. </strong>A lot of tech businesses make a fuck tonne of money
                     because i) very few people on this planet have the privilege of
-                    creating software and ii) it&apos;s not like operating a
-                    restaurant which has a fixed amount of seats/limited IRL access.
-                    For some reason, the unbridled scaling power of software comes
-                    at the cost of a good user experience (what the REAL person on
-                    the ground feels when they use it). <em>Do businesses take we the
-                    people for granted?</em>
+                    creating software (even with AI!) and ii) it&apos;s not like operating a
+                    restaurant which has a fixed amount of seats and limited IRL access. 
+                    {" "}
+                    <em>Do businesses take we the
+                    people for granted?</em> <br /><br />
                   </p>
                 </li>
                 <li>
                   <p>
-                    As culture progresses towards a more people-first system that
-                    corrects the current overindexing on state and business rights
-                    over individual freedom and collective liberation and physical
-                    safety (yes, I&apos;m an optimist - even if transitions are
-                    difficult and take longer than expected), so must our standards
-                    for businesses, business leaders, and generally, vibes (
-                    <em>VOIBES</em>).
+                    <strong>Infrastructure is changing; too quickly, too slowly. </strong>{" "}As culture progresses towards a people-first system that
+                    rebalances overindexing on state and business rights
+                    towards equitable individual freedom and collective liberation (yes, I&apos;m an optimist playing the long game), so must our standards... for everything.
                   </p>
                 </li>
               </ol>
 
               <p style={{ marginTop: 24 }}>
-                In 2020 when the world temporarily shut down, I married new skills
-                with old questions. Markets froze and we were all figuring out how
+                So in 2020 when the world temporarily shut down, I tried to apply new skills
+                with old questions. Life froze and we were all figuring out how
                 to stay sane;
               </p>
-              <ul className="mt-4 list-disc space-y-2 pl-6">
+              <ul
+                className="mt-4 list-disc space-y-2 pl-6"
+                style={{ marginLeft: isMobile ? 12 : 24 }}
+              >
                 <li>
-                  Why does money anxiety feel <em>SO bad?</em>
-                </li>
+                  Why does money anxiety feel <em>so <strong>SO</strong> </em>bad?
+                </li> 
                 <li>
                   What are my richer friends doing that I&apos;m not, other than
                   being born into stability?
-                </li>
+                </li> 
                 <li>How can I do more things with friends?</li>
-                <li>
+                <li> 
                   And most importantly, <em>why am I still so damn lonely?</em>
                 </li>
               </ul>
 
               <p style={{ marginTop: 24 }}>
                 Along the way I had a front row seat to the only tech billionare
-                I&apos;d endorse for President; Kenneth J. Lin. I quietly watched
-                this humble visionary build a $9B business that protected people
+                I&apos;d endorse for President; Kenneth J. Lin. I watched
+                him build a $9B business that protected people
                 from predators, empowered them with transparency and optionality,
                 prioritized employee health and equity, while co-sponsoring an NBA
-                team and PRINTING money for shareholders.
+                team and PRINTING money for shareholders. Quietly, no ego.
               </p>
 
               <p style={{ marginTop: 24 }}>
                 But something nagged at me. First, despite our best efforts to
-                unlock financial progress for 1 out of 3 Americans on our app, the
-                American Dream is not within reach for the majority of Americans.
-                The best technology in the world can&apos;t fix this. It&apos;s a
+                unlock financial progress for many, many people, the
+                American Dream is still not within reach for the majority of folks and no app can fix that.
+               It&apos;s a
                 systemic problem that can be summarized in one chart.
               </p>
               <p style={{ marginTop: 24 }}>
@@ -1022,28 +1017,26 @@ export default function ObjectsHero({ onInvest }: { onInvest: () => void }) {
 
               <p style={{ marginTop: 24 }}>
                 The internet, and now AI, has made shareholder value so easy to
-                unlock but wages haven&apos;t adjusted. Second, the business model
-                was still fundamentally based on ads. I didn&apos;t like that,
-                personally.
+                unlock but wages and social safety nets haven&apos;t evolved at the same rate. The other issue was more personal; I don't like ad-based business models.
               </p>
 
               <p style={{ marginTop: 24 }}>
-                I mean, look at today&apos;s largest consumer software non-retail ad
-                business; Meta. Don&apos;t get me wrong. I <em>have</em> to leave
-                room for diplomacy because I&apos;m trying to be a big girll; and
-                there are many solid people earning great livings from Meta&apos;s
-                payroll, so I don&apos;t wish harm on the business. And, I celebrate
+                I mean, look at today&apos;s largest consumer software (non-retail) ad
+                business; Meta. Don&apos;t get me wrong. I <em>have</em> {" "}to leave
+                room for diplomacy because I&apos;m trying to be a big girl and
+                there are many solid people earning tremendous livings from Meta&apos;s
+                payroll. I don&apos;t wish harm on the business. In fact, I celebrate
                 Wasian babies and the Chan-Zuckerberg kids are gonna cure cancer so,
-                God bless the family. But look at our digital system today. Outside
+                God bless the family. But look at our digital environment today. Outside
                 of chat, our Social tools revolve around an interaction of posting
                 content for external feedback (likes, shares, reposts, follows). The
                 product became a content business, which is great for the brilliant
                 and baddie creatives pioneering careers in a new world, but awful
-                for our attention spans and standards since the machine incentivizes
-                volume and organic content punches through the noise using rage and
+                for our attention spans since the machine incentivizes
+                volume and therefore, organic content must punch through the noise using rage and
                 scandal to be noticed. All of this is a system that normalizes
                 divisiveness & fear & an insiduous depression that hijacks our
-                ability to generate dopamine elsewhere.
+                ability to generate dopamine elsewhere. Also like... we trying to run another election on Facebook adspend, or...? Cap it, my guy.
               </p>
 
               <p style={{ marginTop: 24 }}>
@@ -1051,67 +1044,65 @@ export default function ObjectsHero({ onInvest }: { onInvest: () => void }) {
                 once, all the time.
               </p>
 
-              <p style={{ marginTop: 24 }}>I know. I know you know, too.</p>
-
               <p style={{ marginTop: 24 }}>
-                Our other connection tool is dating. Idk man - I think a lot of us
-                just outgrew the dating apps but don&apos;t really have another
-                choice, especially in a more remote school and work culture.
+                Our other connection tool is dating and... idk. I just think a lot of us
+                 outgrew the dating apps but don&apos;t really have another
+                choice, especially in a more remote culture.
               </p>
 
               <p style={{ marginTop: 24 }}>
-                In 2023, I began my quest to redesign social. After failing to
-                launch a Social x Finance app (a concept warmly dubbed "Karmic
+                So then in 2023, I began my quest to redesign Social. After failing to
+                launch a Social x Finance app (aforementioned prototype called "Karmic
                 Vision"), I left the fintech world in SF and launched an app at a
-                hot social startup in LA. That didn&apos;t work out other. The scales
+                hot social startup in LA. That didn&apos;t work out either. But the scales
                 tipped and I focused next on Online x Offline integrations, then on
-                Social x Dating; what if your friends were included in your dating
-                app experience? People already give each other their phones at
+                Social x Dating. What if your friends were involved in your dating
+                app? People already give each other their phones at
                 brunch or send screenshots in the group chat.
               </p>
 
               <p style={{ marginTop: 24 }}>
-                I spent most of 2024 alchemizing our first round of feedback into a
+                I spent most of 2024 alchemizing the first round of feedback into a
                 functional prototype, iterating with women in LA and Pasadena. By
                 the end of the year, I sunsetted the app to redesign it from the
-                ground up with everything we had learned, rebrand our energy, while
-                beginning to learn how to vibe code shittily with super-early AI
+                ground up with everything we had learned, rebrand our marketing, while
+                learning how to vibe code poorly with super early AI
                 tools until I hit a wall and got professional and specialized help
                 to launch our not-so-basic MVP on the App Store in 2025. Post-launch
                 was a flurry of "oh shit, gotta fix this" while getting folks to try
-                the app. Chaos, but hey, we live!
+                the app. Chaos, but hey, we're live!
               </p>
 
               <p style={{ marginTop: 24 }}>
                 The good news; the most important thing that needed to happen (for
-                that stage) happened. We saw a 2.5x viral coefficient out the gate
+                that stage) happened. In Q1 2026, we saw a 2.5x viral coefficient out the gate
                 without using any paid acquisition, which meant we could reliabily
                 keep growing without forcing a shit tonne of ads down your throat.
                 Personally? Fuck yeah. I don&apos;t ever wanna force someone to
-                loving me or loving my art - and I dont enjoy the limelight so I&apos;d
+                love me or my art - and I dont enjoy the limelight so I&apos;d
                 rather you hear about us through your friends.
               </p>
 
               <p style={{ marginTop: 24 }}>
-                The bad news? Holy shit, there is so much work to do. I need help.
+                The bad news? Holy shit, there is so much work to do. Jury needs help.
               </p>
 
               <p style={{ marginTop: 24 }}>
-                So here we are. If you&apos;ve been following me for a minute, you
+                If you&apos;ve been following me for a minute, you
                 know some other crazy shit happened. Two obsessed, relentless,
                 fanatic stalkers who hacked my shit and shared it with many other
-                people while I was building and starting life over post-marriage.
-                Then the jealous friend of someone I went on 4 dates with who,
-                despite us no longer being active, took it upon themselves to harass
-                me, hijack an early-fledgling business, steal unreleased work as
+                people while I was also starting life over post-marriage.
+                Then the jealous friend of someone I didn't even date, who,
+                despite us no longer talking, took it upon themselves to harass
+                me on the world wide web, hijack an early-fledgling business, steal unreleased work as
                 their own, publicly joke about my hack, threaten to expose intimate
                 content, and just straight up bully me to... notice her? Idk.
-                It&apos;s weird, and not the good kind.
+                It&apos;s weird. Not the good kind.
               </p>
 
               <p style={{ marginTop: 24 }}>
                 So, some drama. A different kind of workplace politics, I guess.
-                Some celeb exposure. Some imaginary players, some big business.
+                Some celeb exposure. Some imaginary players. Some big business at stake.
                 Heavy on existential crisis. But I need to be real with you; all of
                 this is a distraction from the main thing that needs to stay the main
                 thing. We don&apos;t need it. We have something that works,
@@ -1123,13 +1114,13 @@ export default function ObjectsHero({ onInvest }: { onInvest: () => void }) {
 
               <p style={{ marginTop: 24 }}>We just have to get it outside.</p>
 
-              <p style={{ marginTop: 24 }}>Meet me outside?</p>
-
               <p style={{ marginTop: 24 }}>
                 There&apos;s a legendary era just around the corner. At minimum,
                 good hangs with friends, maybe a cheeky cig, and a crush here
                 and/or there 🙃
               </p>
+
+              <p style={{ marginTop: 24 }}>Meet me outside?</p>
             </div>
 
             <div
