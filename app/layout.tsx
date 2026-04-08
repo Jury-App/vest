c1a7a15d64c834ac47723e8b855e44e748771e5f
nicoleym <153690412+nicoleym@users.noreply.github.com>
2026-04-08 13:50:18 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Jury; Private Investment Invite",
+  title: "Jury; Investor Invite",
   description: "Invest in what matters",
   robots: {
     index: false,
